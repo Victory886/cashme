3ac7b1c4b11ecc8c6558440231e132013d0669a9
baosl <shengli.bao@quarkintl.com>
2023-10-26 22:09:31 +0800

    优化导航栏

--- lib/widget/titleBar.dart
+++ lib/widget/titleBar.dart
@@ -2,7 +2,7 @@
  * @Author: Terry
  * @Date: 2023-10-12 15:05:06
  * @LastEditors: Terry
- * @LastEditTime: 2023-10-19 22:47:43
+ * @LastEditTime: 2023-10-25 18:08:43
  * @FilePath: /loannow/lib/widget/titleBar.dart
  */
 import 'package:flutter/material.dart';
@@ -17,6 +17,10 @@ class TitleBar extends StatefulWidget {
   VoidCallback? rightClick;
   String? rightImage;
 
+  /// 是否显示返回按钮
+  bool? isShowBackBtn = true;
+  bool? isShowNaviBottomLine = true;
+
   TitleBar({
     Key? key,
     required this.title,
@@ -26,6 +30,8 @@ class TitleBar extends StatefulWidget {
     this.textColor,
     this.rightClick,
     this.rightImage,
+    this.isShowBackBtn,
+    this.isShowNaviBottomLine,
   }) : super(key: key);
 
   @override
@@ -35,68 +41,127 @@ class TitleBar extends StatefulWidget {
 }
 
 class TitleBarState extends State<TitleBar> {
+  Widget _navBackWiget() {
+    return widget.isShowBackBtn ?? true
+        ? InkWell(
+            highlightColor: Colors.white,
+            splashColor: Colors.transparent,
+            onTap: () {
+              if (widget.leftClick != null) {
+                widget.leftClick!();
+              } else {
+                Navigator.pop(context);
+              }
+            },
+            child: Container(
+              alignment: Alignment.center,
+              width: double.infinity,
+              child: Image.asset(
+                widget.leftImage ?? "images/ic_back.png",
+                width: 40,
+                height: 20,
+              ),
+            ),
+          )
+        : Container();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: widget.bgColor ?? Colors.white),
-      child: SafeArea(
-        child: Container(
-          decoration: const BoxDecoration(
-            border: Border(
-              bottom: BorderSide(color: Color(0xFFE9E9E9), width: 0.5, style: BorderStyle.solid),
+    return AppBar(
+      title: Text(
+        widget.title,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+        style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16, color: widget.textColor ?? AppColors.textColor),
+      ),
+      elevation: (widget.isShowNaviBottomLine ?? true) ? 0.3 : 0,
+      leading: _navBackWiget(),
+
+      backgroundColor: widget.bgColor ?? Colors.white,
+      actions: [
+        if (widget.rightImage != null)
+          Align(
+            alignment: Alignment.centerRight,
+            child: InkWell(
+              onTap: () {
+                if (widget.rightClick != null) widget.rightClick!();
+              },
+              child: Container(
+                padding: const EdgeInsets.all(13),
+                child: Image.asset(widget.rightImage!),
+              ),
             ),
           ),
-          height: 44,
-          child: Stack(
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: InkWell(
-                  onTap: () {
-                    if (widget.leftClick != null) {
-                      widget.leftClick!();
-                    } else {
-                      Navigator.pop(context);
-                    }
-                    // widget.leftClick ?? Navigator.pop(context);
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.all(13),
-                    child: Image.asset(
-                      widget.leftImage ?? "images/ic_back.png",
-                    ),
-                  ),
-                ),
-              ),
-              Center(
-                child: Container(
-                  alignment: Alignment.center,
-                  width: 200,
-                  child: Text(
-                    widget.title,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16, color: widget.textColor ?? AppColors.textColor),
-                  ),
-                ),
-              ),
-              if (widget.rightImage != null)
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: InkWell(
-                    onTap: () {
-                      if (widget.rightClick != null) widget.rightClick!();
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(13),
-                      child: Image.asset(widget.rightImage!),
-                    ),
-                  ),
-                ),
-            ],
-          ),
-        ),
-      ),
+      ],
+      // )
+      // Container(
+      //   decoration: BoxDecoration(color: widget.bgColor ?? Colors.white),
+
+      //   child: SafeArea(
+      //     child: Container(
+      //       decoration: const BoxDecoration(
+      //         border: Border(
+      //           bottom: BorderSide(
+      //             color: Color(0xFFE9E9E9),
+      //             width: 0.5,
+      //             style: BorderStyle.solid,
+      //           ),
+      //         ),
+      //       ),
+      //       height: 44,
+      //       child: Stack(
+      //         children: [
+      // if (widget.ishowBackBtn ?? true)
+      //   Align(
+      //     alignment: Alignment.centerLeft,
+      //     child: InkWell(
+      //       onTap: () {
+      //         if (widget.leftClick != null) {
+      //           widget.leftClick!();
+      //         } else {
+      //           Navigator.pop(context);
+      //         }
+      //         // widget.leftClick ?? Navigator.pop(context);
+      //       },
+      //       child: Container(
+      //         padding: const EdgeInsets.all(13),
+      //         child: Image.asset(
+      //           widget.leftImage ?? "images/ic_back.png",
+      //           // fit: BoxFit.fitWidth,
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      //           Center(
+      //             child: Container(
+      //               alignment: Alignment.center,
+      //               width: 200,
+      //               child: Text(
+      //                 widget.title,
+      // maxLines: 1,
+      // overflow: TextOverflow.ellipsis,
+      // style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16, color: widget.textColor ?? AppColors.textColor),
+      //               ),
+      //             ),
+      //           ),
+      // if (widget.rightImage != null)
+      //   Align(
+      //     alignment: Alignment.centerRight,
+      //     child: InkWell(
+      //       onTap: () {
+      //         if (widget.rightClick != null) widget.rightClick!();
+      //       },
+      //       child: Container(
+      //         padding: const EdgeInsets.all(13),
+      //         child: Image.asset(widget.rightImage!),
+      //       ),
+      //     ),
+      //   ),
+      //         ],
+      //       ),
+      //     ),
+      //   ),
     );
   }
 }
