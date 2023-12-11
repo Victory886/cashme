5213bdec38ffcdcb541f1d802da06dfe8cb9367a
baosl <shengli.bao@quarkintl.com>
2023-12-11 17:02:24 +0800

    替换引号

--- lib/generated/json/account_delete_bean.g.dart
+++ lib/generated/json/account_delete_bean.g.dart
@@ -1,13 +1,20 @@
+/*
+ * @Author: Terry
+ * @Date: 2023-10-12 15:05:06
+ * @LastEditors: Terry
+ * @LastEditTime: 2023-12-11 17:02:07
+ * @FilePath: /loannow/lib/generated/json/account_delete_bean.g.dart
+ */
 import 'package:loannow/generated/json/base/json_convert_content.dart';
 import 'package:loannow/beans/account_delete_bean.dart';
 
 AccountDeleteBean $AccountDeleteBeanFromJson(Map<String, dynamic> json) {
   final AccountDeleteBean accountDeleteBean = AccountDeleteBean();
-  final int? id = jsonConvert.convert<int>(json['id']);
+  final int? id = jsonConvert.convert<int>(json["id"]);
   if (id != null) {
     accountDeleteBean.id = id;
   }
-  final String? accountNo = jsonConvert.convert<String>(json['accountNo']);
+  final String? accountNo = jsonConvert.convert<String>(json["accountNo"]);
   if (accountNo != null) {
     accountDeleteBean.accountNo = accountNo;
   }
@@ -16,8 +23,8 @@ AccountDeleteBean $AccountDeleteBeanFromJson(Map<String, dynamic> json) {
 
 Map<String, dynamic> $AccountDeleteBeanToJson(AccountDeleteBean entity) {
   final Map<String, dynamic> data = <String, dynamic>{};
-  data['id'] = entity.id;
-  data['accountNo'] = entity.accountNo;
+  data["id"] = entity.id;
+  data["accountNo"] = entity.accountNo;
   return data;
 }
 
