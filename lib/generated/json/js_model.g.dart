a980fab3b58d0ab1a8079adf5598e4f8aae962b3
baosl <shengli.bao@quarkintl.com>
2023-12-11 17:16:52 +0800

    替换引号

--- lib/generated/json/js_model.g.dart
+++ lib/generated/json/js_model.g.dart
@@ -1,17 +1,24 @@
+/*
+ * @Author: Terry
+ * @Date: 2023-11-27 15:23:03
+ * @LastEditors: Terry
+ * @LastEditTime: 2023-12-11 17:16:37
+ * @FilePath: /loannow/lib/generated/json/js_model.g.dart
+ */
 import 'package:loannow/generated/json/base/json_convert_content.dart';
 import 'package:loannow/generated/js_model.dart';
 
 JsModel $JsModelFromJson(Map<String, dynamic> json) {
   final JsModel jsModel = JsModel();
-  final String? handlerName = jsonConvert.convert<String>(json['handlerName']);
+  final String? handlerName = jsonConvert.convert<String>(json["handlerName"]);
   if (handlerName != null) {
     jsModel.handlerName = handlerName;
   }
-  final JsModelData? data = jsonConvert.convert<JsModelData>(json['data']);
+  final JsModelData? data = jsonConvert.convert<JsModelData>(json["data"]);
   if (data != null) {
     jsModel.data = data;
   }
-  final String? callbackId = jsonConvert.convert<String>(json['callbackId']);
+  final String? callbackId = jsonConvert.convert<String>(json["callbackId"]);
   if (callbackId != null) {
     jsModel.callbackId = callbackId;
   }
@@ -20,9 +27,9 @@ JsModel $JsModelFromJson(Map<String, dynamic> json) {
 
 Map<String, dynamic> $JsModelToJson(JsModel entity) {
   final Map<String, dynamic> data = <String, dynamic>{};
-  data['handlerName'] = entity.handlerName;
-  data['data'] = entity.data?.toJson();
-  data['callbackId'] = entity.callbackId;
+  data["handlerName"] = entity.handlerName;
+  data["data"] = entity.data?.toJson();
+  data["callbackId"] = entity.callbackId;
   return data;
 }
 
@@ -41,21 +48,20 @@ extension JsModelExtension on JsModel {
 
 JsModelData $JsModelDataFromJson(Map<String, dynamic> json) {
   final JsModelData jsModelData = JsModelData();
-  final String? uid = jsonConvert.convert<String>(json['uid']);
+  final String? uid = jsonConvert.convert<String>(json["uid"]);
   if (uid != null) {
     jsModelData.uid = uid;
   }
-  final Map<String, dynamic>? params =
-  (json['params'] as Map<String, dynamic>?)?.map(
-          (k, e) => MapEntry(k, e == null ? null : e));
+  final Map<String, dynamic>? params = (json["params"] as Map<String, dynamic>?)
+      ?.map((k, e) => MapEntry(k, e == null ? null : e));
   if (params != null) {
     jsModelData.params = params;
   }
-  final String? method = jsonConvert.convert<String>(json['method']);
+  final String? method = jsonConvert.convert<String>(json["method"]);
   if (method != null) {
     jsModelData.method = method;
   }
-  final String? callback = jsonConvert.convert<String>(json['callback']);
+  final String? callback = jsonConvert.convert<String>(json["callback"]);
   if (callback != null) {
     jsModelData.callback = callback;
   }
@@ -64,10 +70,10 @@ JsModelData $JsModelDataFromJson(Map<String, dynamic> json) {
 
 Map<String, dynamic> $JsModelDataToJson(JsModelData entity) {
   final Map<String, dynamic> data = <String, dynamic>{};
-  data['uid'] = entity.uid;
-  data['params'] = entity.params;
-  data['method'] = entity.method;
-  data['callback'] = entity.callback;
+  data["uid"] = entity.uid;
+  data["params"] = entity.params;
+  data["method"] = entity.method;
+  data["callback"] = entity.callback;
   return data;
 }
 
