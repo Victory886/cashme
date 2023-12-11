3653b31831f1c44b099a293d9a2f1c11ce2bbcc5
baosl <shengli.bao@quarkintl.com>
2023-12-11 16:00:54 +0800

    替换引号

--- lib/generated/json/loan_history_bean.g.dart
+++ lib/generated/json/loan_history_bean.g.dart
@@ -1,35 +1,42 @@
+/*
+ * @Author: Terry
+ * @Date: 2023-10-12 15:05:06
+ * @LastEditors: Terry
+ * @LastEditTime: 2023-12-11 16:00:34
+ * @FilePath: /loannow/lib/generated/json/loan_history_bean.g.dart
+ */
 import 'package:loannow/generated/json/base/json_convert_content.dart';
 import 'package:loannow/beans/loan_history_bean.dart';
 
 LoanHistoryBean $LoanHistoryBeanFromJson(Map<String, dynamic> json) {
   final LoanHistoryBean loanHistoryBean = LoanHistoryBean();
-  final int? applicationTime = jsonConvert.convert<int>(
-      json['applicationTime']);
+  final int? applicationTime =
+      jsonConvert.convert<int>(json["applicationTime"]);
   if (applicationTime != null) {
     loanHistoryBean.applicationTime = applicationTime;
   }
-  final int? id = jsonConvert.convert<int>(json['id']);
+  final int? id = jsonConvert.convert<int>(json["id"]);
   if (id != null) {
     loanHistoryBean.id = id;
   }
-  final int? loanAmount = jsonConvert.convert<int>(json['loanAmount']);
+  final int? loanAmount = jsonConvert.convert<int>(json["loanAmount"]);
   if (loanAmount != null) {
     loanHistoryBean.loanAmount = loanAmount;
   }
-  final int? loanBasisInfoId = jsonConvert.convert<int>(
-      json['loanBasisInfoId']);
+  final int? loanBasisInfoId =
+      jsonConvert.convert<int>(json["loanBasisInfoId"]);
   if (loanBasisInfoId != null) {
     loanHistoryBean.loanBasisInfoId = loanBasisInfoId;
   }
-  final int? loanTimeLimit = jsonConvert.convert<int>(json['loanTimeLimit']);
+  final int? loanTimeLimit = jsonConvert.convert<int>(json["loanTimeLimit"]);
   if (loanTimeLimit != null) {
     loanHistoryBean.loanTimeLimit = loanTimeLimit;
   }
-  final int? status = jsonConvert.convert<int>(json['status']);
+  final int? status = jsonConvert.convert<int>(json["status"]);
   if (status != null) {
     loanHistoryBean.status = status;
   }
-  final String? statusDesc = jsonConvert.convert<String>(json['statusDesc']);
+  final String? statusDesc = jsonConvert.convert<String>(json["statusDesc"]);
   if (statusDesc != null) {
     loanHistoryBean.statusDesc = statusDesc;
   }
@@ -38,13 +45,13 @@ LoanHistoryBean $LoanHistoryBeanFromJson(Map<String, dynamic> json) {
 
 Map<String, dynamic> $LoanHistoryBeanToJson(LoanHistoryBean entity) {
   final Map<String, dynamic> data = <String, dynamic>{};
-  data['applicationTime'] = entity.applicationTime;
-  data['id'] = entity.id;
-  data['loanAmount'] = entity.loanAmount;
-  data['loanBasisInfoId'] = entity.loanBasisInfoId;
-  data['loanTimeLimit'] = entity.loanTimeLimit;
-  data['status'] = entity.status;
-  data['statusDesc'] = entity.statusDesc;
+  data["applicationTime"] = entity.applicationTime;
+  data["id"] = entity.id;
+  data["loanAmount"] = entity.loanAmount;
+  data["loanBasisInfoId"] = entity.loanBasisInfoId;
+  data["loanTimeLimit"] = entity.loanTimeLimit;
+  data["status"] = entity.status;
+  data["statusDesc"] = entity.statusDesc;
   return data;
 }
 
