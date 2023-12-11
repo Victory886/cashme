515f2b202c3e60ded261c143bcee5b33038d02c9
baosl <shengli.bao@quarkintl.com>
2023-12-11 15:53:52 +0800

    添加logo、修改app名字、替换引号

--- lib/generated/json/basis_info_bean.g.dart
+++ lib/generated/json/basis_info_bean.g.dart
@@ -3,181 +3,180 @@ import 'package:loannow/beans/basis_info_bean.dart';
 
 BasisInfoBean $BasisInfoBeanFromJson(Map<String, dynamic> json) {
   final BasisInfoBean basisInfoBean = BasisInfoBean();
-  final String? accountNo = jsonConvert.convert<String>(json['accountNo']);
+  final String? accountNo = jsonConvert.convert<String>(json["accountNo"]);
   if (accountNo != null) {
     basisInfoBean.accountNo = accountNo;
   }
-  final String? bank = jsonConvert.convert<String>(json['bank']);
+  final String? bank = jsonConvert.convert<String>(json["bank"]);
   if (bank != null) {
     basisInfoBean.bank = bank;
   }
-  final String? bankCode = jsonConvert.convert<String>(json['bankCode']);
+  final String? bankCode = jsonConvert.convert<String>(json["bankCode"]);
   if (bankCode != null) {
     basisInfoBean.bankCode = bankCode;
   }
-  final String? bankLogo = jsonConvert.convert<String>(json['bankLogo']);
+  final String? bankLogo = jsonConvert.convert<String>(json["bankLogo"]);
   if (bankLogo != null) {
     basisInfoBean.bankLogo = bankLogo;
   }
-  final int? birthday = jsonConvert.convert<int>(json['birthday']);
+  final int? birthday = jsonConvert.convert<int>(json["birthday"]);
   if (birthday != null) {
     basisInfoBean.birthday = birthday;
   }
-  final int? certificateMaterial = jsonConvert.convert<int>(
-      json['certificateMaterial']);
+  final int? certificateMaterial =
+      jsonConvert.convert<int>(json["certificateMaterial"]);
   if (certificateMaterial != null) {
     basisInfoBean.certificateMaterial = certificateMaterial;
   }
-  final String? certificateNo = jsonConvert.convert<String>(
-      json['certificateNo']);
+  final String? certificateNo =
+      jsonConvert.convert<String>(json["certificateNo"]);
   if (certificateNo != null) {
     basisInfoBean.certificateNo = certificateNo;
   }
-  final int? certificateType = jsonConvert.convert<int>(
-      json['certificateType']);
+  final int? certificateType =
+      jsonConvert.convert<int>(json["certificateType"]);
   if (certificateType != null) {
     basisInfoBean.certificateType = certificateType;
   }
-  final String? companyName = jsonConvert.convert<String>(json['companyName']);
+  final String? companyName = jsonConvert.convert<String>(json["companyName"]);
   if (companyName != null) {
     basisInfoBean.companyName = companyName;
   }
-  final String? companyTelephone = jsonConvert.convert<String>(
-      json['companyTelephone']);
+  final String? companyTelephone =
+      jsonConvert.convert<String>(json["companyTelephone"]);
   if (companyTelephone != null) {
     basisInfoBean.companyTelephone = companyTelephone;
   }
-  final List<
-      BasisInfoContactPersons>? contactPersons = (json['contactPersons'] as List<
-      dynamic>?)?.map(
-          (e) =>
-      jsonConvert.convert<BasisInfoContactPersons>(
-          e) as BasisInfoContactPersons).toList();
+  final List<BasisInfoContactPersons>? contactPersons =
+      (json["contactPersons"] as List<dynamic>?)
+          ?.map((e) => jsonConvert.convert<BasisInfoContactPersons>(e)
+              as BasisInfoContactPersons)
+          .toList();
   if (contactPersons != null) {
     basisInfoBean.contactPersons = contactPersons;
   }
-  final String? deviceId = jsonConvert.convert<String>(json['deviceId']);
+  final String? deviceId = jsonConvert.convert<String>(json["deviceId"]);
   if (deviceId != null) {
     basisInfoBean.deviceId = deviceId;
   }
-  final int? education = jsonConvert.convert<int>(json['education']);
+  final int? education = jsonConvert.convert<int>(json["education"]);
   if (education != null) {
     basisInfoBean.education = education;
   }
-  final String? email = jsonConvert.convert<String>(json['email']);
+  final String? email = jsonConvert.convert<String>(json["email"]);
   if (email != null) {
     basisInfoBean.email = email;
   }
-  final String? fbAccount = jsonConvert.convert<String>(json['fbAccount']);
+  final String? fbAccount = jsonConvert.convert<String>(json["fbAccount"]);
   if (fbAccount != null) {
     basisInfoBean.fbAccount = fbAccount;
   }
-  final String? firstName = jsonConvert.convert<String>(json['firstName']);
+  final String? firstName = jsonConvert.convert<String>(json["firstName"]);
   if (firstName != null) {
     basisInfoBean.firstName = firstName;
   }
-  final int? id = jsonConvert.convert<int>(json['id']);
+  final int? id = jsonConvert.convert<int>(json["id"]);
   if (id != null) {
     basisInfoBean.id = id;
   }
-  final int? incomeId = jsonConvert.convert<int>(json['incomeId']);
+  final int? incomeId = jsonConvert.convert<int>(json["incomeId"]);
   if (incomeId != null) {
     basisInfoBean.incomeId = incomeId;
   }
-  final int? industry = jsonConvert.convert<int>(json['industry']);
+  final int? industry = jsonConvert.convert<int>(json["industry"]);
   if (industry != null) {
     basisInfoBean.industry = industry;
   }
-  final String? insAccount = jsonConvert.convert<String>(json['insAccount']);
+  final String? insAccount = jsonConvert.convert<String>(json["insAccount"]);
   if (insAccount != null) {
     basisInfoBean.insAccount = insAccount;
   }
-  final String? lastName = jsonConvert.convert<String>(json['lastName']);
+  final String? lastName = jsonConvert.convert<String>(json["lastName"]);
   if (lastName != null) {
     basisInfoBean.lastName = lastName;
   }
-  final int? loanStep = jsonConvert.convert<int>(json['loanStep']);
+  final int? loanStep = jsonConvert.convert<int>(json["loanStep"]);
   if (loanStep != null) {
     basisInfoBean.loanStep = loanStep;
   }
-  final int? maritalStatus = jsonConvert.convert<int>(json['maritalStatus']);
+  final int? maritalStatus = jsonConvert.convert<int>(json["maritalStatus"]);
   if (maritalStatus != null) {
     basisInfoBean.maritalStatus = maritalStatus;
   }
-  final int? marketBasisId = jsonConvert.convert<int>(json['marketBasisId']);
+  final int? marketBasisId = jsonConvert.convert<int>(json["marketBasisId"]);
   if (marketBasisId != null) {
     basisInfoBean.marketBasisId = marketBasisId;
   }
-  final int? marketMemberId = jsonConvert.convert<int>(json['marketMemberId']);
+  final int? marketMemberId = jsonConvert.convert<int>(json["marketMemberId"]);
   if (marketMemberId != null) {
     basisInfoBean.marketMemberId = marketMemberId;
   }
-  final int? memberId = jsonConvert.convert<int>(json['memberId']);
+  final int? memberId = jsonConvert.convert<int>(json["memberId"]);
   if (memberId != null) {
     basisInfoBean.memberId = memberId;
   }
-  final String? memberPhone = jsonConvert.convert<String>(json['memberPhone']);
+  final String? memberPhone = jsonConvert.convert<String>(json["memberPhone"]);
   if (memberPhone != null) {
     basisInfoBean.memberPhone = memberPhone;
   }
-  final String? middleName = jsonConvert.convert<String>(json['middleName']);
+  final String? middleName = jsonConvert.convert<String>(json["middleName"]);
   if (middleName != null) {
     basisInfoBean.middleName = middleName;
   }
-  final String? name = jsonConvert.convert<String>(json['name']);
+  final String? name = jsonConvert.convert<String>(json["name"]);
   if (name != null) {
     basisInfoBean.name = name;
   }
-  final String? nameFormat = jsonConvert.convert<String>(json['nameFormat']);
+  final String? nameFormat = jsonConvert.convert<String>(json["nameFormat"]);
   if (nameFormat != null) {
     basisInfoBean.nameFormat = nameFormat;
   }
-  final int? paymentChannelType = jsonConvert.convert<int>(
-      json['paymentChannelType']);
+  final int? paymentChannelType =
+      jsonConvert.convert<int>(json["paymentChannelType"]);
   if (paymentChannelType != null) {
     basisInfoBean.paymentChannelType = paymentChannelType;
   }
-  final int? phPhoneVerify = jsonConvert.convert<int>(json['phPhoneVerify']);
+  final int? phPhoneVerify = jsonConvert.convert<int>(json["phPhoneVerify"]);
   if (phPhoneVerify != null) {
     basisInfoBean.phPhoneVerify = phPhoneVerify;
   }
-  final int? platform = jsonConvert.convert<int>(json['platform']);
+  final int? platform = jsonConvert.convert<int>(json["platform"]);
   if (platform != null) {
     basisInfoBean.platform = platform;
   }
-  final String? positivePhoto = jsonConvert.convert<String>(
-      json['positivePhoto']);
+  final String? positivePhoto =
+      jsonConvert.convert<String>(json["positivePhoto"]);
   if (positivePhoto != null) {
     basisInfoBean.positivePhoto = positivePhoto;
   }
-  final String? positivePhotoUrl = jsonConvert.convert<String>(
-      json['positivePhotoUrl']);
+  final String? positivePhotoUrl =
+      jsonConvert.convert<String>(json["positivePhotoUrl"]);
   if (positivePhotoUrl != null) {
     basisInfoBean.positivePhotoUrl = positivePhotoUrl;
   }
-  final int? resideAreaId = jsonConvert.convert<int>(json['resideAreaId']);
+  final int? resideAreaId = jsonConvert.convert<int>(json["resideAreaId"]);
   if (resideAreaId != null) {
     basisInfoBean.resideAreaId = resideAreaId;
   }
-  final String? resideBarangay = jsonConvert.convert<String>(
-      json['resideBarangay']);
+  final String? resideBarangay =
+      jsonConvert.convert<String>(json["resideBarangay"]);
   if (resideBarangay != null) {
     basisInfoBean.resideBarangay = resideBarangay;
   }
-  final String? resideCity = jsonConvert.convert<String>(json['resideCity']);
+  final String? resideCity = jsonConvert.convert<String>(json["resideCity"]);
   if (resideCity != null) {
     basisInfoBean.resideCity = resideCity;
   }
-  final String? resideProvince = jsonConvert.convert<String>(
-      json['resideProvince']);
+  final String? resideProvince =
+      jsonConvert.convert<String>(json["resideProvince"]);
   if (resideProvince != null) {
     basisInfoBean.resideProvince = resideProvince;
   }
-  final int? resideTime = jsonConvert.convert<int>(json['resideTime']);
+  final int? resideTime = jsonConvert.convert<int>(json["resideTime"]);
   if (resideTime != null) {
     basisInfoBean.resideTime = resideTime;
   }
-  final int? sex = jsonConvert.convert<int>(json['sex']);
+  final int? sex = jsonConvert.convert<int>(json["sex"]);
   if (sex != null) {
     basisInfoBean.sex = sex;
   }
@@ -186,48 +185,48 @@ BasisInfoBean $BasisInfoBeanFromJson(Map<String, dynamic> json) {
 
 Map<String, dynamic> $BasisInfoBeanToJson(BasisInfoBean entity) {
   final Map<String, dynamic> data = <String, dynamic>{};
-  data['accountNo'] = entity.accountNo;
-  data['bank'] = entity.bank;
-  data['bankCode'] = entity.bankCode;
-  data['bankLogo'] = entity.bankLogo;
-  data['birthday'] = entity.birthday;
-  data['certificateMaterial'] = entity.certificateMaterial;
-  data['certificateNo'] = entity.certificateNo;
-  data['certificateType'] = entity.certificateType;
-  data['companyName'] = entity.companyName;
-  data['companyTelephone'] = entity.companyTelephone;
-  data['contactPersons'] =
+  data["accountNo"] = entity.accountNo;
+  data["bank"] = entity.bank;
+  data["bankCode"] = entity.bankCode;
+  data["bankLogo"] = entity.bankLogo;
+  data["birthday"] = entity.birthday;
+  data["certificateMaterial"] = entity.certificateMaterial;
+  data["certificateNo"] = entity.certificateNo;
+  data["certificateType"] = entity.certificateType;
+  data["companyName"] = entity.companyName;
+  data["companyTelephone"] = entity.companyTelephone;
+  data["contactPersons"] =
       entity.contactPersons?.map((v) => v.toJson()).toList();
-  data['deviceId'] = entity.deviceId;
-  data['education'] = entity.education;
-  data['email'] = entity.email;
-  data['fbAccount'] = entity.fbAccount;
-  data['firstName'] = entity.firstName;
-  data['id'] = entity.id;
-  data['incomeId'] = entity.incomeId;
-  data['industry'] = entity.industry;
-  data['insAccount'] = entity.insAccount;
-  data['lastName'] = entity.lastName;
-  data['loanStep'] = entity.loanStep;
-  data['maritalStatus'] = entity.maritalStatus;
-  data['marketBasisId'] = entity.marketBasisId;
-  data['marketMemberId'] = entity.marketMemberId;
-  data['memberId'] = entity.memberId;
-  data['memberPhone'] = entity.memberPhone;
-  data['middleName'] = entity.middleName;
-  data['name'] = entity.name;
-  data['nameFormat'] = entity.nameFormat;
-  data['paymentChannelType'] = entity.paymentChannelType;
-  data['phPhoneVerify'] = entity.phPhoneVerify;
-  data['platform'] = entity.platform;
-  data['positivePhoto'] = entity.positivePhoto;
-  data['positivePhotoUrl'] = entity.positivePhotoUrl;
-  data['resideAreaId'] = entity.resideAreaId;
-  data['resideBarangay'] = entity.resideBarangay;
-  data['resideCity'] = entity.resideCity;
-  data['resideProvince'] = entity.resideProvince;
-  data['resideTime'] = entity.resideTime;
-  data['sex'] = entity.sex;
+  data["deviceId"] = entity.deviceId;
+  data["education"] = entity.education;
+  data["email"] = entity.email;
+  data["fbAccount"] = entity.fbAccount;
+  data["firstName"] = entity.firstName;
+  data["id"] = entity.id;
+  data["incomeId"] = entity.incomeId;
+  data["industry"] = entity.industry;
+  data["insAccount"] = entity.insAccount;
+  data["lastName"] = entity.lastName;
+  data["loanStep"] = entity.loanStep;
+  data["maritalStatus"] = entity.maritalStatus;
+  data["marketBasisId"] = entity.marketBasisId;
+  data["marketMemberId"] = entity.marketMemberId;
+  data["memberId"] = entity.memberId;
+  data["memberPhone"] = entity.memberPhone;
+  data["middleName"] = entity.middleName;
+  data["name"] = entity.name;
+  data["nameFormat"] = entity.nameFormat;
+  data["paymentChannelType"] = entity.paymentChannelType;
+  data["phPhoneVerify"] = entity.phPhoneVerify;
+  data["platform"] = entity.platform;
+  data["positivePhoto"] = entity.positivePhoto;
+  data["positivePhotoUrl"] = entity.positivePhotoUrl;
+  data["resideAreaId"] = entity.resideAreaId;
+  data["resideBarangay"] = entity.resideBarangay;
+  data["resideCity"] = entity.resideCity;
+  data["resideProvince"] = entity.resideProvince;
+  data["resideTime"] = entity.resideTime;
+  data["sex"] = entity.sex;
   return data;
 }
 
@@ -322,42 +321,43 @@ extension BasisInfoBeanExtension on BasisInfoBean {
 
 BasisInfoContactPersons $BasisInfoContactPersonsFromJson(
     Map<String, dynamic> json) {
-  final BasisInfoContactPersons basisInfoContactPersons = BasisInfoContactPersons();
-  final int? basisInfoId = jsonConvert.convert<int>(json['basisInfoId']);
+  final BasisInfoContactPersons basisInfoContactPersons =
+      BasisInfoContactPersons();
+  final int? basisInfoId = jsonConvert.convert<int>(json["basisInfoId"]);
   if (basisInfoId != null) {
     basisInfoContactPersons.basisInfoId = basisInfoId;
   }
-  final int? createTime = jsonConvert.convert<int>(json['createTime']);
+  final int? createTime = jsonConvert.convert<int>(json["createTime"]);
   if (createTime != null) {
     basisInfoContactPersons.createTime = createTime;
   }
-  final int? id = jsonConvert.convert<int>(json['id']);
+  final int? id = jsonConvert.convert<int>(json["id"]);
   if (id != null) {
     basisInfoContactPersons.id = id;
   }
-  final String? name = jsonConvert.convert<String>(json['name']);
+  final String? name = jsonConvert.convert<String>(json["name"]);
   if (name != null) {
     basisInfoContactPersons.name = name;
   }
-  final String? relationship = jsonConvert.convert<String>(
-      json['relationship']);
+  final String? relationship =
+      jsonConvert.convert<String>(json["relationship"]);
   if (relationship != null) {
     basisInfoContactPersons.relationship = relationship;
   }
-  final String? telephoneNo = jsonConvert.convert<String>(json['telephoneNo']);
+  final String? telephoneNo = jsonConvert.convert<String>(json["telephoneNo"]);
   if (telephoneNo != null) {
     basisInfoContactPersons.telephoneNo = telephoneNo;
   }
-  final int? telephoneNoHash = jsonConvert.convert<int>(
-      json['telephoneNoHash']);
+  final int? telephoneNoHash =
+      jsonConvert.convert<int>(json["telephoneNoHash"]);
   if (telephoneNoHash != null) {
     basisInfoContactPersons.telephoneNoHash = telephoneNoHash;
   }
-  final int? type = jsonConvert.convert<int>(json['type']);
+  final int? type = jsonConvert.convert<int>(json["type"]);
   if (type != null) {
     basisInfoContactPersons.type = type;
   }
-  final int? userid = jsonConvert.convert<int>(json['userid']);
+  final int? userid = jsonConvert.convert<int>(json["userid"]);
   if (userid != null) {
     basisInfoContactPersons.userid = userid;
   }
@@ -367,15 +367,15 @@ BasisInfoContactPersons $BasisInfoContactPersonsFromJson(
 Map<String, dynamic> $BasisInfoContactPersonsToJson(
     BasisInfoContactPersons entity) {
   final Map<String, dynamic> data = <String, dynamic>{};
-  data['basisInfoId'] = entity.basisInfoId;
-  data['createTime'] = entity.createTime;
-  data['id'] = entity.id;
-  data['name'] = entity.name;
-  data['relationship'] = entity.relationship;
-  data['telephoneNo'] = entity.telephoneNo;
-  data['telephoneNoHash'] = entity.telephoneNoHash;
-  data['type'] = entity.type;
-  data['userid'] = entity.userid;
+  data["basisInfoId"] = entity.basisInfoId;
+  data["createTime"] = entity.createTime;
+  data["id"] = entity.id;
+  data["name"] = entity.name;
+  data["relationship"] = entity.relationship;
+  data["telephoneNo"] = entity.telephoneNo;
+  data["telephoneNoHash"] = entity.telephoneNoHash;
+  data["type"] = entity.type;
+  data["userid"] = entity.userid;
   return data;
 }
 
