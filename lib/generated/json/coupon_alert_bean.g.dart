76199640159588c10c5ade95c9ac36f96e12c3a8
baosl <shengli.bao@quarkintl.com>
2023-12-11 17:08:28 +0800

    替换引号

--- lib/generated/json/coupon_alert_bean.g.dart
+++ lib/generated/json/coupon_alert_bean.g.dart
@@ -1,38 +1,45 @@
+/*
+ * @Author: Terry
+ * @Date: 2023-11-29 17:00:52
+ * @LastEditors: Terry
+ * @LastEditTime: 2023-12-11 17:08:11
+ * @FilePath: /loannow/lib/generated/json/coupon_alert_bean.g.dart
+ */
 import 'package:loannow/generated/json/base/json_convert_content.dart';
 import 'package:loannow/beans/coupon_alert_bean.dart';
 
 CouponAlertBean $CouponAlertBeanFromJson(Map<String, dynamic> json) {
   final CouponAlertBean couponAlertBean = CouponAlertBean();
-  final double? amount = jsonConvert.convert<double>(json['amount']);
+  final double? amount = jsonConvert.convert<double>(json["amount"]);
   if (amount != null) {
     couponAlertBean.amount = amount;
   }
-  final int? amountType = jsonConvert.convert<int>(json['amountType']);
+  final int? amountType = jsonConvert.convert<int>(json["amountType"]);
   if (amountType != null) {
     couponAlertBean.amountType = amountType;
   }
-  final int? createTime = jsonConvert.convert<int>(json['createTime']);
+  final int? createTime = jsonConvert.convert<int>(json["createTime"]);
   if (createTime != null) {
     couponAlertBean.createTime = createTime;
   }
-  final int? endUsableTime = jsonConvert.convert<int>(json['endUsableTime']);
+  final int? endUsableTime = jsonConvert.convert<int>(json["endUsableTime"]);
   if (endUsableTime != null) {
     couponAlertBean.endUsableTime = endUsableTime;
   }
-  final int? id = jsonConvert.convert<int>(json['id']);
+  final int? id = jsonConvert.convert<int>(json["id"]);
   if (id != null) {
     couponAlertBean.id = id;
   }
-  final int? sourceType = jsonConvert.convert<int>(json['sourceType']);
+  final int? sourceType = jsonConvert.convert<int>(json["sourceType"]);
   if (sourceType != null) {
     couponAlertBean.sourceType = sourceType;
   }
-  final int? startUsableTime = jsonConvert.convert<int>(
-      json['startUsableTime']);
+  final int? startUsableTime =
+      jsonConvert.convert<int>(json["startUsableTime"]);
   if (startUsableTime != null) {
     couponAlertBean.startUsableTime = startUsableTime;
   }
-  final int? status = jsonConvert.convert<int>(json['status']);
+  final int? status = jsonConvert.convert<int>(json["status"]);
   if (status != null) {
     couponAlertBean.status = status;
   }
@@ -41,14 +48,14 @@ CouponAlertBean $CouponAlertBeanFromJson(Map<String, dynamic> json) {
 
 Map<String, dynamic> $CouponAlertBeanToJson(CouponAlertBean entity) {
   final Map<String, dynamic> data = <String, dynamic>{};
-  data['amount'] = entity.amount;
-  data['amountType'] = entity.amountType;
-  data['createTime'] = entity.createTime;
-  data['endUsableTime'] = entity.endUsableTime;
-  data['id'] = entity.id;
-  data['sourceType'] = entity.sourceType;
-  data['startUsableTime'] = entity.startUsableTime;
-  data['status'] = entity.status;
+  data["amount"] = entity.amount;
+  data["amountType"] = entity.amountType;
+  data["createTime"] = entity.createTime;
+  data["endUsableTime"] = entity.endUsableTime;
+  data["id"] = entity.id;
+  data["sourceType"] = entity.sourceType;
+  data["startUsableTime"] = entity.startUsableTime;
+  data["status"] = entity.status;
   return data;
 }
 
